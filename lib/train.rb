ca1627f59e332cff6c536ab865c0bb8844428bbb
stephen <lonelytreesl@yahoo.com>
2015-06-10 14:13:37 -0700

    add spec and code for Train.all() to make it pass, all specs passing.

--- lib/train.rb
+++ lib/train.rb
@@ -5,4 +5,14 @@ class Train
   define_method(:initialize) do |attributes|
     @line = attributes.fetch(:line)
   end
+
+  define_singleton_method(:all) do
+    all_trains = DB.exec("SELECT * FROM trains;")
+    trains = []
+    all_trains.each do |train|
+      line = train.fetch("line")
+      trains.push(Train.new(:line => line))
+    end
+    trains
+  end
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -7,4 +7,10 @@ describe(Train) do
       expect(test_train.line()).to(eq("Blue"))
     end
   end
+
+  describe('.all') do
+    it('returns an array of saved instances of Train, it is empty at first') do
+      expect(Train.all()).to(eq([]))
+    end
+  end
 end
