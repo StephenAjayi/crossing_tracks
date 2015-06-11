c1912502f503ca99edcdb2b9661a66933c775391
Guest User <student@epicodus.com>
2015-06-11 12:00:25 -0700

    add spec and code for City#name() to make it pass, all specs passing.

--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -1,1 +1,9 @@
+require('spec_helper')
 
+describe(City)
+describe('#name') do
+  it('returns the name of an instance of city') do
+    test_city = City.new(:name => "Portland")
+    expect(test_city.name()).to(eq("Portland"))
+  end
+end
