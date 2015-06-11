19427ae170cc9d3f0a68a025edb2dd4d540b26c2
Guest User <student@epicodus.com>
2015-06-11 12:11:55 -0700

    add spec and code for City.all() to make it pass, all specs passing.

--- lib/city.rb
+++ lib/city.rb
@@ -5,4 +5,14 @@ class City
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
   end
+
+  define_singleton_method(:all) do
+    cities = []
+    found_cities = DB.exec("SELECT * FROM cities;")
+    found_cities.each() do |found_city|
+      name = found_city.fetch("name")
+      cities.push(City.new(:name => "name"))
+    end
+    cities
+  end
 end
--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -1,9 +1,16 @@
 require('spec_helper')
 
-describe(City)
-describe('#name') do
-  it('returns the name of an instance of city') do
-    test_city = City.new(:name => "Portland")
-    expect(test_city.name()).to(eq("Portland"))
+describe(City) do
+  describe('#name') do
+    it('returns the name of an instance of city') do
+      test_city = City.new(:name => "Portland")
+      expect(test_city.name()).to(eq("Portland"))
+    end
+  end
+
+  describe(".all") do
+    it('returns all saved instances of City in an array, it starts out empty at first') do
+      expect(City.all()).to(eq([]))
+    end
   end
 end
