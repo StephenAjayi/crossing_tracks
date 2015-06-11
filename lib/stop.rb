55070a96aca699d89e43a39a05d45d62dac15515
Guest User <student@epicodus.com>
2015-06-11 15:39:57 -0700

    add spec and code for Stop#train_id() to make it pass, all specs passing.

--- lib/stop.rb
+++ lib/stop.rb
@@ -1,8 +1,9 @@
 class Stop
 
-  attr_reader(:city_id)
+  attr_reader(:city_id, :train_id)
 
   define_method(:initialize) do |attributes|
     @city_id = attributes.fetch(:city_id)
+    @train_id = attributes.fetch(:train_id)
   end
 end
--- spec/stop_spec.rb
+++ spec/stop_spec.rb
@@ -3,8 +3,15 @@ require('spec_helper')
 describe(Stop) do
   describe('#city_id') do
     it("returns the id of a stops city") do
-      test_stop = Stop.new(:city_id => 1)
+      test_stop = Stop.new(:city_id => 1, :train_id => 1)
       expect(test_stop.city_id()).to(eq(1))
+    end
+  end
+
+  describe('#train_id') do
+    it('returns the id of a train that stops at a stop') do
+      test_stop = Stop.new(:city_id => 1, :train_id => 1)
+      expect(test_stop.train_id()).to(eq(1))
     end
   end
 end
